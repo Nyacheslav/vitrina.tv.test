602610db70882cfbe70879241f44eae4f58d5575
Vyacheslav <vakulenkovyacheslav@gmail.com>
2020-09-12 12:06:49 +0300

    Initial Commit

---
+++ vitrina.tv.test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vitrina.tv.test
+//
+//  Created by Vyacheslav Vakulenko on 12.09.2020.
+//  Copyright © 2020 nyacheslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
